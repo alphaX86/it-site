9cdefd948bf6d0f56c2a83006e458fce91480b97
shankuroko <shankarsar9942@gmail.com>
2021-09-01 09:47:07 +0530

    okay

--- faculty.php
+++ faculty.php
@@ -1,1 +1,15 @@
+<!DOCTYPE html>
+<html lang='en'>
+    <head>
+        <script src="https://kit.fontawesome.com/d294cf5192.js" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href='./CSS/main.css'>
+    </head>
+    <body>
+    <div class='site'>
+        <?php include 'header.php'?>
+
+    </div>
+    </body>
+
+</html>
 
