3aaf102b0fa64b8d5fc8c9fad8d349adf14d167d
DeepshikaR <rdeepshi13@gmail.com>
2021-11-02 16:44:09 +0530

    Placements - Updated

--- placement.php
+++ placement.php
@@ -4,6 +4,7 @@
         <script src="https://kit.fontawesome.com/d294cf5192.js" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
         <link rel="stylesheet" href='./css/main.css'>
+        
     </head>
     <body>
                
@@ -26,24 +27,52 @@
                             </figcaption>
 	                        </figure>
                         </div>
-                        <div style="padding:15px">
+                        <div style="padding:13px; background-color: rgba(255, 255, 255, 0.8)">
                             <h3>COMPANIES</h3>
                             <table>
                                 <colgroup>
-                                    <col style="width:50%">
-                                    <col style="width:50%">
+                                    <col style="width:25%">
+                                    <col style="width:25%">
+                                    <col style="width:25%">
+                                    <col style="width:25%">
                                 </colgroup>  
-                                <tr><td>- Wells Fargo</td>    
-                                    <td>- Citicorp</td>
+                                <tr><td>Accenture</td>    
+                                    <td>Accolite</td>
+                                    <td>Adobe</td>
+                                    <td>Bank of America</td>
                                 </tr>
-                                <tr><td>- Wells Fargo</td>    
-                                    <td>- Citicorp</td>
+                                <tr><td>Barclays</td>
+                                    <td>Caterpillar</td>
+                                    <td>Citicorp</td>    
+                                    <td>CTS</td>
                                 </tr>
-                                <tr><td>- Wells Fargo</td>    
-                                    <td>- Citicorp</td>
+                                <tr><td>IBM</td>
+                                    <td>KLA Tencor</td>    
+                                    <td>Lister Technologies</td>
+                                    <td>L&T</td>
                                 </tr>
-                                <tr><td>- Wells Fargo</td>    
-                                    <td>- Citicorp</td>
+                                <tr><td>Natwest</td>    
+                                    <td>Navis India Ltd.</td>
+                                    <td>NielsenIQ</td>
+                                    <td>Oracle</td>
+                                </tr>
+                                <tr><td>Publicis Sapients</td>
+                                    <td>RBS</td>
+                                    <td>Samsung</td>    
+                                    <td>SAP Labs</td>
+                                </tr>
+                                <tr><td>Searce</td>
+                                    <td>Silicon Valley Bank</td>    
+                                    <td>TCS</td>
+                                    <td>Tekion</td>
+                                </tr>
+                                <tr><td>Verizon</td>    
+                                    <td>Visa</td>
+                                    <td>Walmart Labs</td>
+                                    <td>Wells Fargo</td>
+                                </tr>
+                                <tr><td>Wipro</td>
+                                    <td>Zoho</td>
                                 </tr>
                             </table> 
                         </div>
@@ -83,7 +112,7 @@
                             }
                         </script>
                     </div>
-                    <br><br>
+                    <br><br> 
 
                     <!-- BAR CHART FINAL - DON'T TOUCH THIS -->
                     <h2>PREVIOUS YEARS PLACEMENT STASTICS</h2>  
@@ -101,18 +130,18 @@
                                 label: '# placed',
                                 data: [79, 75, 85, 77, 87, 87, 95, 95, 88, 103, 88, 65],
                                 backgroundColor: [
-                                    'rgba(255, 99, 132, 0.2)',
-                                    'rgba(54, 162, 235, 0.2)',
-                                    'rgba(255, 206, 86, 0.2)',
-                                    'rgba(75, 192, 192, 0.2)',
-                                    'rgba(153, 102, 255, 0.2)',
-                                    'rgba(255, 159, 64, 0.2)',
-                                    'rgba(255, 99, 132, 0.2)',
-                                    'rgba(54, 162, 235, 0.2)',
-                                    'rgba(255, 206, 86, 0.2)',
-                                    'rgba(75, 192, 192, 0.2)',
-                                    'rgba(153, 102, 255, 0.2)',
-                                    'rgba(255, 159, 64, 0.2)'
+                                    'rgba(255, 99, 132, 0.7)',
+                                    'rgba(54, 162, 235, 0.7)',
+                                    'rgba(255, 206, 86, 0.7)',
+                                    'rgba(75, 192, 192, 0.7)',
+                                    'rgba(153, 102, 255, 0.7)',
+                                    'rgba(255, 159, 64, 0.7)',
+                                    'rgba(255, 99, 132, 0.7)',
+                                    'rgba(54, 162, 235, 0.7)',
+                                    'rgba(255, 206, 86, 0.7)',
+                                    'rgba(75, 192, 192, 0.7)',
+                                    'rgba(153, 102, 255, 0.7)',
+                                    'rgba(255, 159, 64, 0.7)'
                                 ],
                                 borderColor: [
                                     'rgba(255, 99, 132, 1)',
@@ -140,6 +169,110 @@
                         }
                     });
                     </script>
+                    <p style="text-align:center; color: grey;">Academic Year (Number of Students Registered)</p>
+                    <br><br>
+                    
+                    <h2>STUDENTS OPTED FOR HIGHER STUDIES</h2>  
+                    <table class="table-striped" style="background-color: rgba(255, 255, 255, 0.6)">
+                        <colgroup>
+                            <col style="width:40%">
+                            <col style="width:20%">
+                            <col style="width:20%">
+                            <col style="width:20%">
+                        </colgroup>      
+                        <thead>
+                            <tr><th>Year</th>
+                                <th>Ph.D</th>
+                                <th>MS</th>
+                                <th>MBA </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr><td>2018 - 2019</td>
+                                <td>-</td> <td>2</td> <td>-</td>
+                            </tr>    
+                            <tr><td>2017 - 2018</td>
+                                <td>-</td> <td>5</td> <td>-</td>
+                            </tr>  
+                            <tr><td>2016 - 2017</td>
+                                <td>-</td> <td>3</td> <td>1</td>
+                            </tr>
+                            <tr><td>2015 - 2016</td>
+                                <td>-</td> <td>4</td> <td>1</td>
+                            </tr>
+                            <tr><td>2014 - 2015</td>
+                                <td>-</td> <td>9</td> <td>-</td>
+                            </tr>
+                            <tr><td>2013 - 2014</td>
+                                <td>-</td> <td>-</td> <td>-</td>
+                            </tr>
+                            <tr><td>2012 - 2013</td>
+                                <td>1</td> <td>6</td> <td>-</td>
+                            </tr>
+                            <tr><td>2011 - 2012</td>
+                                <td>-</td> <td>1</td> <td>-</td>
+                            </tr>
+                            <tr><td>2010 - 2011</td>
+                                <td>-</td> <td>2</td> <td>2</td>
+                            </tr>
+                            <tr><td>2009 - 2010</td>
+                                <td>-</td> <td>-</td> <td>-</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <br><br>
+
+                    <h2>HIGHER STUDIES DETAILS</h2>  
+                    <div class="highers">
+                        <div class="hYears">
+                            <div class="hYrs" onclick="makeVisible('y1819')">
+                                <p>2018 - 2019</p>
+                            </div>
+                            <div class="hYrs" onclick="makeVisible('y1718')">
+                                <p>2017 - 2018</p>
+                            </div>
+                            <div class="hYrs" onclick="makeVisible('y1617')">
+                                <p>2016 - 2017</p>
+                            </div>
+                            <div class="hYrs" onclick="makeVisible('y1516')">
+                                <p>2015 - 2016</p>
+                            </div>  
+                            <div class="hYrs" onclick="makeVisible('y1415')">
+                                <p>2014 - 2015</p>
+                            </div>     
+                        </div>
+
+                        <div class="highers-list" id="y1819" >
+                        <?php                    
+                            $sql ='SELECT * FROM Highers WHERE `year` = 2019';
+                            $result = $conn->query($sql);
+                            $i=0;
+                            echo '<table class="table-striped" style="background-color: rgba(255, 255, 255, 0.6)">';
+                            echo '<thead>
+                                    <tr><th>S.No</th>
+                                        <th>Year of Passing</th>
+                                        <th>Name</th>
+                                        <th>Program Name</th>
+                                        <th>University</th>
+                                    </tr>
+                                </thead>';
+                            echo '<tbody>';
+                            while($row = $result->fetch_assoc()) {
+                                $Year=$row['year'];
+                                $Name=$row['name'];
+                                $Prog=$row['program'];
+                                $Uni=$row['university'];
+                                $i=$i+1;
+                                echo '<tr><td>`'.$i.'`</td>
+                                          <td>`'.$Year.'`</td>
+                                          <td>`'.$Name.'`</td>
+                                          <td>`'.$Prog.'`</td>
+                                          <td>`'.$Uni.'`</td></tr>';
+                            }
+                            echo '</tbody';
+                        ?>       
+                        </div>
+                    </div>
                     <br><br>
 
                 </div>
