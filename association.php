bcfb5be7abd0602dac7ece2ead9c1c274de36a15
DeepshikaR <rdeepshi13@gmail.com>
2021-12-09 22:38:56 +0530

    Finished ITA Page

--- association.php
+++ association.php
@@ -3,6 +3,28 @@
     <head>
         <script src="https://kit.fontawesome.com/d294cf5192.js" crossorigin="anonymous"></script>
         <link rel="stylesheet" href='./CSS/main.css'>
+        <script>
+        var yearIds = ["y1819", "y1718", "y1617", "y1516"];
+        var  currentVisible = null;
+        function toggleVisibility(divId) 
+        {   if(currentVisible === divId) 
+            {   currentVisible = null;  } 
+            else 
+            {   currentVisible = divId; }
+            hideNonVisibleDivs();
+        }
+        function hideNonVisibleDivs() 
+        {   var i, divId, div;
+            for(i = 0; i < yearIds.length; i++) 
+            {   divId = yearIds[i];
+                div = document.getElementById(divId);
+                if(currentVisible === divId) 
+                {   div.style.display = "block"; } 
+                else 
+                {   div.style.display = "none";  }
+            }
+        }
+        </script>
     </head>
 
     <body>
@@ -73,13 +95,126 @@
                             </div>
                             <div class='gen-info-content'>
                                 <h2>Annual Events</h2>
-                                <p>. MUTEX, an intra college technical festival.The main intention of MUTEX is to focus on the juniors and train them to hone their technical and analytical skills. In view of this, guest lectures on personality development and leadership will be arranged and various technical activities and events will be conducted<br><br>
-                                   . SAMHITA , a national-level technical symposium.. SAMHITA’19 will be a 2-day fest comprising of both onsite and online events. It will offer more than 20 events on various technological levels. It also includes workshops on the latest IT trends.
+                                <p>  MUTEX, an intra college technical festival.The main intention of MUTEX is to focus on the juniors and train them to hone their technical and analytical skills. In view of this, guest lectures on personality development and leadership will be arranged and various technical activities and events will be conducted<br><br>
+                                     SAMHITA , a national-level technical symposium.. SAMHITA’19 will be a 2-day fest comprising of both onsite and online events. It will offer more than 20 events on various technological levels. It also includes workshops on the latest IT trends.
                                 </p>
+                            </div>
+                        </div>
+                        <br><br>
+
+                        <h2>EVENTS CONDUCTED IN PREVIOUS YEARS</h2>
+                        <div class='assoc-act-eve'>
+                            <div class='gen-info-content'>
+                                <h2>Technical Events</h2>
+                                <table style="text-align:center">
+                                    <tbody>
+                                        <tr><td>Programing Contests</td>
+                                            <td>Pursuit of Prodigies</td></tr>
+                                        <tr><td>Code Obfuscation</td>
+                                            <td>Bit Coding</td></tr>
+                                        <tr><td>Ace the Interview</td>
+                                            <td>Reverse Coding</td></tr>
+                                        <tr><td>Coffee with Java</td>
+                                            <td>Debugging</td></tr>
+                                        <tr><td>Web Designing</td>
+                                            <td>Mega Event</td></tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                            <div class='gen-info-content'>
+                                <h2>Non-Technical Events</h2>
+                                <table style="text-align:center">
+                                    <tbody>
+                                        <tr><td>Sudoku</td>
+                                            <td>Logo Quiz</td></tr>
+                                        <tr><td>Mobile Games</td>
+                                            <td>Connexions</td></tr>
+                                        <tr><td>Crossword</td>
+                                            <td>Photography</td></tr>
+                                        <tr><td>Google Trotter</td>
+                                            <td>Capture the Flag</td></tr>
+                                        <tr><td>General Quiz</td>
+                                            <td>Math O' Mania</td></tr>
+                                    </tbody>
+                                </table> 
+                            </div>
+                        </div>
+                        <br><br>
+
+                        <h2>ACTIVITIES CONDUCTED IN PREVIOUS YEARS</h2>
+                        <div class="highers">
+                            <div class="hYears">
+                                <div class="hYrs" onclick="toggleVisibility('y1819')">
+                                    <p>2018 - 2019</p>
+                                </div>
+                                <div class="hYrs" onclick="toggleVisibility('y1718')">
+                                    <p>2017 - 2018</p>
+                                </div>
+                                <div class="hYrs" onclick="toggleVisibility('y1617')">
+                                    <p>2016 - 2017</p>
+                                </div>
+                                <div class="hYrs" onclick="toggleVisibility('y1516')">
+                                    <p>2015 - 2016</p>
+                                </div>                                 
+                            </div>
+
+                            <div class="highers-list" id="y1819" >
+                            <table class="table-striped-assoc" style="background-color: rgba(255, 255, 255, 0.6)">
+                            <tbody>
+                                    <tr><td>A guest Lecture on Role of IT @ CERN by Mr.S.P Mohanty was organized</td></tr>
+                                    <tr><td>A guest Lecture on Ethical Hacking By Mr.Ankit Fadia, a leading ethical hacking enthusiast from India</td></tr>
+                                    <tr><td>A seminar on Entrepreneurship by Mr.Suhas Gopinath, who is recognized as the World’s Youngest CEO, was held</td></tr>
+                                    <tr><td>The ITA also proposes to have a Guest Lecture on Lyrics Engineering by Dr.V.Madhan Karky, Founder of Karefo Foundation</td></tr>
+                                </tbody>
+                            </table>
+                            </div>
+
+                            <div class="highers-list" id="y1718" >
+                            <table class="table-striped-assoc" style="background-color: rgba(255, 255, 255, 0.6)">
+                            <tbody>
+                                    <tr><td>A guest Lecture on Role of IT @ CERN by Mr.S.P Mohanty was organized</td></tr>
+                                    <tr><td>A guest Lecture on Ethical Hacking By Mr.Ankit Fadia, a leading ethical hacking enthusiast from India</td></tr>
+                                    <tr><td>A seminar on Entrepreneurship by Mr.Suhas Gopinath, who is recognized as the World’s Youngest CEO, was held</td></tr>
+                                    <tr><td>The ITA also proposes to have a Guest Lecture on Lyrics Engineering by Dr.V.Madhan Karky, Founder of Karefo Foundation</td></tr>
+                                </tbody>
+                            </table>
+                            </div>
+
+                            <div class="highers-list" id="y1617" >
+                            <table class="table-striped-assoc" style="background-color: rgba(255, 255, 255, 0.6)">
+                            <tbody>
+                                    <tr><td>A guest Lecture on Role of IT @ CERN by Mr.S.P Mohanty was organized</td></tr>
+                                    <tr><td>A guest Lecture on Ethical Hacking By Mr.Ankit Fadia, a leading ethical hacking enthusiast from India</td></tr>
+                                    <tr><td>A seminar on Entrepreneurship by Mr.Suhas Gopinath, who is recognized as the World’s Youngest CEO, was held</td></tr>
+                                    <tr><td>The ITA also proposes to have a Guest Lecture on Lyrics Engineering by Dr.V.Madhan Karky, Founder of Karefo Foundation</td></tr>
+                                </tbody>
+                            </table>
+                            </div>
+
+                            <div class="highers-list" id="y1516" >
+                            <table class="table-striped-assoc" style="background-color: rgba(255, 255, 255, 0.6)">
+                                <tbody>
+                                    <tr><td>A guest Lecture on Role of IT @ CERN by Mr.S.P Mohanty was organized</td></tr>
+                                    <tr><td>A guest Lecture on Ethical Hacking By Mr.Ankit Fadia, a leading ethical hacking enthusiast from India</td></tr>
+                                    <tr><td>A seminar on Entrepreneurship by Mr.Suhas Gopinath, who is recognized as the World’s Youngest CEO, was held</td></tr>
+                                    <tr><td>The ITA also proposes to have a Guest Lecture on Lyrics Engineering by Dr.V.Madhan Karky, Founder of Karefo Foundation</td></tr>
+                                </tbody>
+                            </table>
                             </div>
 
                         </div>
+                        <br><br>
 
+                        <h2>ITA GALLERY</h2>
+                        <div class='assoc-gallery'>
+                            <div><img src="./Images/Gallery/13_ITAGallery/ita8.JPG"></img></div>
+                            <div><img src="./Images/Gallery/13_ITAGallery/ita12.JPG"></img></div>
+                            <div><img src="./Images/Gallery/13_ITAGallery/ita13.JPG"></img></div>
+                            <div><img src="./Images/Gallery/13_ITAGallery/ita14.JPG"></img></div>
+                            <div><img src="./Images/Gallery/13_ITAGallery/ita15.JPG"></img></div>
+                            <div><img src="./Images/Gallery/13_ITAGallery/ita22.JPG"></img></div>
+                        </div>
+                        <br><br>
 
 
 
